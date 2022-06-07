a4c19ee1ea575a7994411f38752ef6fef3ea0e87
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-07 22:03:17 +0200

    Update package.d

--- source/models/warehouse/entities/package.d
+++ source/models/warehouse/entities/package.d
@@ -1,4 +1,5 @@
 module models.warehouse.entities;
 
 public import models.warehouse.entities.containers;
-public import models.warehouse.entities.inventorys;
+public import models.warehouse.entities.inbounds;
+public import models.warehouse.entities.inventories;
